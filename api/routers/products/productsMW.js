9e8211971859fee577370bd0913036562f642140
JorshuaG <joshbgearheart@gmail.com>
2022-01-07 16:19:18 -1000

    added middleware to prevent repeat category entries to DB

--- api/routers/products/productsMW.js
+++ api/routers/products/productsMW.js
@@ -1,1 +1,18 @@
+const Products = require("./productsModel.js");
 
+const categoryUnique = async (req, res, next) => {
+  try {
+    const existingCategory = await Products.getProductCategoryByName(req.body);
+    if (existingCategory) {
+      next({ message: "that name already exists", status: 400 });
+    } else {
+      next();
+    }
+  } catch (err) {
+    next(err);
+  }
+};
+
+module.exports = {
+  categoryUnique,
+};
