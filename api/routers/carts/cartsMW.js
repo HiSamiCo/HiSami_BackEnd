4420af1e0ccb004bc9ccaa9c730b3bf26ca432aa
JorshuaG <joshbgearheart@gmail.com>
2022-01-14 14:11:04 -1000

    started working on stripe implementation

--- api/routers/carts/cartsMW.js
+++ api/routers/carts/cartsMW.js
@@ -1,32 +1,64 @@
-const Carts = require("./cartsModel")
-const cartSchema = require("../../schemas/cart")
+const Carts = require("./cartsModel");
+const cartSchema = require("../../schemas/cart");
+const Products = require("../products/productsModel");
 
 const validateCartPayload = async (req, res, next) => {
-    try {
-        const { body } = req
-        await cartSchema.validate(body)
-        next()
-    } catch (err) {
-        next(err)
-    }
-}
+  try {
+    const { body } = req;
+    await cartSchema.validate(body);
+    next();
+  } catch (err) {
+    next(err);
+  }
+};
 
 const cartItemExists = async (req, res, next) => {
-    try {
-        const { cart_item_id } = req.params
-        const { subject: user_id } = req.sentUser
-        const [cartItem] = await Carts.getCartItemBy({ cart_item_id, user_id })
-        if(cartItem) {
-            next()
-        } else {
-            next({ message: "This item is not in cart", status: 400 })
-        }   
-    } catch(err) {
-        next(err)
+  try {
+    const { cart_item_id } = req.params;
+    const { subject: user_id } = req.sentUser;
+    const [cartItem] = await Carts.getCartItemBy({ cart_item_id, user_id });
+    if (cartItem) {
+      next();
+    } else {
+      next({ message: "This item is not in cart", status: 400 });
     }
-}
+  } catch (err) {
+    next(err);
+  }
+};
+
+const validateUserCart = async (req, res, next) => {
+  try {
+    const { subject: user_id } = req.sentUser;
+    const userCart = await Carts.getUserCart(user_id);
+    const errors = userCart.reduce(async (errs, item, index) => {
+      const product = await Products.getProductById(item.product_id);
+      const stock = product.stock - item.quantity;
+      userCart[index] = { ...item, newStock: stock };
+      if (stock < 0) {
+        return [
+          ...errs,
+          {
+            status: 400,
+            message: `Sorry we are out of ${product.product_name}, please check back later`,
+          },
+        ];
+      } else {
+        return errs;
+      }
+    }, []);
+    if (errors.length > 0) {
+      next(errors[0]);
+    }
+    req.userCart = userCart;
+    next();
+  } catch (err) {
+    next(err);
+  }
+};
 
 module.exports = {
-    validateCartPayload,
-    cartItemExists
-}
+  validateCartPayload,
+  cartItemExists,
+  validateUserCart,
+};
--- api/routers/carts/cartsRouter.js
+++ api/routers/carts/cartsRouter.js
@@ -1,51 +1,55 @@
-const router = require("express").Router()
-const Carts = require("./cartsModel")
-const MW = require("./cartsMW")
-// const Products = require("../products/productsModel")
+const router = require("express").Router();
+const Carts = require("./cartsModel");
+const MW = require("./cartsMW");
 
 router.get("/", async (req, res, next) => {
-    try {
-        const cart = await Carts.getUserCart(req.sentUser.subject)
-        res.status(200).json(cart) 
-    } catch(err) {
-        next(err)
-    }
-})
+  try {
+    const cart = await Carts.getUserCart(req.sentUser.subject);
+    res.status(200).json(cart);
+  } catch (err) {
+    next(err);
+  }
+});
 
-// const product = await Products.getProductById(body.product_id)
-// const stock =  product.stock - body.quantity
-// await Products.updateProduct(body.product_id, { ...product, stock })
 router.post("/create", MW.validateCartPayload, async (req, res, next) => {
-    try {
-        const { body, sentUser } = req
-        const { subject: user_id } = sentUser
-        const cart = { ...body, user_id }
-        const createdCart = await Carts.addCart(cart)
-        res.status(201).json(createdCart)
-    } catch(err) {
-        next(err)
-    }
-})
+  try {
+    const { body, sentUser } = req;
+    const { subject: user_id } = sentUser;
+    const cart = { ...body, user_id };
+    const createdCart = await Carts.addCart(cart);
+    res.status(201).json(createdCart);
+  } catch (err) {
+    next(err);
+  }
+});
 
-router.put("/update/:cart_item_id", MW.cartItemExists, async (req, res, next) => {
+router.put(
+  "/update/:cart_item_id",
+  MW.cartItemExists,
+  async (req, res, next) => {
     try {
-        const { body } = req
-        const { cart_item_id } = req.params 
-        const updatedCart = await Carts.updateCartItem(cart_item_id, body)
-        res.status(200).json(updatedCart)
-    } catch(err) {
-        next(err)
+      const { body } = req;
+      const { cart_item_id } = req.params;
+      const updatedCart = await Carts.updateCartItem(cart_item_id, body);
+      res.status(200).json(updatedCart);
+    } catch (err) {
+      next(err);
     }
-})
+  }
+);
 
-router.delete("/delete/:cart_item_id", MW.cartItemExists, async (req, res, next) => {
+router.delete(
+  "/delete/:cart_item_id",
+  MW.cartItemExists,
+  async (req, res, next) => {
     try {
-        const { cart_item_id } = req.params
-        const removedCart = await Carts.removeCartItem(cart_item_id)
-        res.status(200).json(removedCart)
-    } catch(err) {
-        next(err)
+      const { cart_item_id } = req.params;
+      const removedCart = await Carts.removeCartItem(cart_item_id);
+      res.status(200).json(removedCart);
+    } catch (err) {
+      next(err);
     }
-})
+  }
+);
 
-module.exports = router
+module.exports = router;
