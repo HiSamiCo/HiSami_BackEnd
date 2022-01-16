fdbfbca5f94506b50d62e00fe0ac8afa334d3cb9
collerhatguy <67057792+collerhatguy@users.noreply.github.com>
2022-01-16 14:20:09 -0600

    added carts endpoint tests

--- api/__tests__/users.test.js
+++ api/__tests__/users.test.js
@@ -3,6 +3,10 @@ const server = require("../server");
 const db = require("../data/db-config");
 const agent = request.agent(server)
 
+jest.mock("../token-builder", () => ({
+  tokenBuilder: () => "this is your token"
+}))
+
 jest.mock("bcrypt", () => ({
   hashSync: (password) => password
 }))
@@ -31,11 +35,17 @@ describe("server.js", () => {
 describe("GET /api/users", () => {
   it("returns all users", async () => {
     const users = await agent.get("/api/users")
-    expect(users).toBeTruthy()
+    expect(users.body).toBeTruthy()
   });
 });
 
-const testUser = { password: "password", first_name: "first_name", last_name: "last_name", email: "email@email.com" }
+const testUser = { 
+  password: "password", 
+  first_name: "first_name", 
+  last_name: "last_name", 
+  email: "email@email.com" 
+}
+
 const expected = { 
   ...testUser, 
   admin: false,
@@ -45,8 +55,20 @@ const expected = {
 describe("POST /api/users/create", () => {
   it("returns an added user", async () => {
     const createdUser = await agent.post("/api/users/create").send(testUser)
-    expect(createdUser).toBeTruthy()
-    console.table(createdUser.body)
+    expect(createdUser.status).toBe(201)
     expect(createdUser.body).toMatchObject(expected)
   })
 })
+
+// const loginUser = { 
+//   email: "fake@fake.com", 
+//   password: "password" 
+// }
+
+// describe("POST /api/users/login", () => {
+//   it("returns a token", async () => {
+//     const token = await agent.post("/api/users/login").send(loginUser)
+//     expect(token.status).toBe(201)
+//     expect(token.body).toBe("")
+//   })
+// })
